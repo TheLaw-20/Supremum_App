7dc67c845a223ae93cfa60c63a9071a78080446d
TheLaw-20 <lawrence21maurice@gmail.com>
2024-12-28 05:20:58 +0200

    search page

--- supremum_app/lib/screen/search_view.dart
+++ supremum_app/lib/screen/search_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:supremum_app/authentication/widget/textfield.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
@@ -6,7 +7,23 @@ class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      
+      body: Column(
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Row(crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: CustomTextField(
+                  hinttext: 'Search',
+                  icon: Icons.search_outlined,
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
